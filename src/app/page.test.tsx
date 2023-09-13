db7930f6dc03227b04a8b5c6377ab34e935c5d03
Bogdan-Klimov-EPAM <Bogdan_Klimov@epam.com>
2023-09-14 01:14:25 +0300

    setup basic jest env and add git actions

---
+++ src/app/page.test.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import Home from "./page";
+
+it("should contain text", () => {
+  const component = render(<Home />);
+
+  expect(component.getByTestId("testText")).toHaveTextContent(
+    "Find in-depth information about Next.js features and API."
+  );
+});
