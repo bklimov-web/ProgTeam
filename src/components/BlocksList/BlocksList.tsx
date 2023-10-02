4b36741865ddafca1afdcd5d7001f6516ae6cf3a
Bogdan-Klimov-EPAM <Bogdan_Klimov@epam.com>
2023-10-02 20:07:23 +0300

    fix blocksList

--- src/components/BlocksList/BlocksList.tsx
+++ src/components/BlocksList/BlocksList.tsx
@@ -1,7 +1,5 @@
 import { Block } from "@prisma/client";
-import { prisma } from "app/db";
 import TextBlock from "components/TextBlock";
-import { revalidatePath } from "next/cache";
 import { FC } from "react";
 
 type Props = {
@@ -15,17 +13,14 @@ const BlocksList: FC<Props> = ({ blocks, projectId }) => {
       {blocks.map(({ description, title, subtitle, id }) => {
         const content = { description, subtitle, title, id };
 
-        const handleDelete = async () => {
-          "use server";
-
-          await prisma.block.delete({
-            where: { id, projectId: projectId },
-          });
-
-          revalidatePath(`/project/${projectId}`);
-        };
-
-        return <TextBlock key={id} content={content} onDelete={handleDelete} />;
+        return (
+          <TextBlock
+            key={id}
+            projectId={+projectId}
+            id={+id}
+            content={content}
+          />
+        );
       })}
     </section>
   );
