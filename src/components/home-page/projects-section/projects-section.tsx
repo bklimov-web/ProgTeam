5db8e90ccd76dd30f4253f2380606d84c14483d8
Bogdan-Klimov-EPAM <Bogdan_Klimov@epam.com>
2023-10-12 01:44:00 +0300

    get rid of the errors

--- src/components/home-page/projects-section/projects-section.tsx
+++ src/components/home-page/projects-section/projects-section.tsx
@@ -1,9 +1,5 @@
-"use client";
-
-import { deleteProject } from "lib/actions/project-actions";
-import { XSquare } from "lucide-react";
-import Link from "next/link";
 import { FC } from "react";
+import ProjectCard from "./project-card";
 
 type Props = {
   projects: any[];
@@ -14,21 +10,7 @@ const ProjectsSection: FC<Props> = ({ projects }) => {
     <section className="flex items-center justify-center gap-[20px]">
       {projects.map(({ name, _id }) => {
         const id = _id.toString();
-
-        return (
-          <div key={id} className="relative cursor-pointer">
-            <XSquare
-              onClick={() => deleteProject(id)}
-              className="absolute right-2 top-2"
-            />
-            <Link
-              className="flex justify-center items-center w-[100px] h-[100px] bg-blue-200"
-              href={`project/${id}`}
-            >
-              {name}
-            </Link>
-          </div>
-        );
+        return <ProjectCard name={name} id={id} />;
       })}
     </section>
   );
